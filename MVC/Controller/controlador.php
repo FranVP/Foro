0528e6452300fe5192dbfee6e9b423873982afe6
Fran Villarreal <Fran_villarreal119@hotmail.com>
2018-01-24 19:57:21 +0100

    MVC

---
+++ MVC/Controller/controlador.php
@@ -0,0 +1,7 @@
+<?php
+//Llamada al modelo
+require_once("Model/model.php");
+//Llamada a la vista
+require_once("View/vista.php");
+
+?>
---
+++ MVC/View/vista.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+    <title>Foro Emagister</title>
+</head>
+<body>
+<h1 align="center">Foro Emagister<br></h1>
+
+<button>
+    <a href="comentarios.php"> Escribir un post </a>
+</button>
+<br>
+<button>
+    <a href="comentarios.php"> Escribir un post</a>
+</button>
+</body>
+</html>
